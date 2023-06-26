b34a4b06521c865c427587dce0179182d1b76f58
linhvnde <linh.nh.tyty@gmail.com>
2023-06-26 17:18:09 +0200

    adjusted Navbar.js name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 
 function App() {
